8fcd42ac6e2b43afff208526209519412c1a745c
ToseDera <josegiovannyverap@gmail.com>
2025-10-23 11:58:39 -0500

    feat: Remove 'Usuarios' link from sidebar for improved navigation

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -13,7 +13,6 @@
             <a href="{{ route('dashboard') }}" class="block px-6 py-3 hover:bg-blue-700">🏠 Dashboard</a>
             <a href="{{ route('vuelos.index') }}" class="block px-6 py-3 hover:bg-blue-700">🛫 Vuelos</a>
             <a href="{{ route('pasajeros.index') }}" class="block px-6 py-3 hover:bg-blue-700">🛫 Pasajeros</a>
-            <a href="#" class="block px-6 py-3 hover:bg-blue-700">👤 Usuarios</a>
             @if (Auth::user()->rol == 2)
                 <a href="{{ route('ciudades.index') }}" class="block px-6 py-3 hover:bg-blue-700">⚙ Ciudades</a>
                 <a href="{{ route('admin.reservas.index') }}" class="block px-6 py-3 hover:bg-blue-700">🎟 Reservas</a>
